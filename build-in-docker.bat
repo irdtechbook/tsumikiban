e85c755741f07ee338343bfb69bfc2232480674f
Takashi Yamashiro <44048851+irdtechbook@users.noreply.github.com>
2022-08-06 17:52:40 +0900

    フォルダ変更

--- build-in-docker.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-docker run --rm -v %cd%:/book vvakame/review /bin/sh -c "cd /book && yarn && yarn build"
-pause
--- build-in-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
-# docker run -i -t -v $(pwd):/book vvakame/review /bin/bash
-
-docker run -t --rm -v $(pwd):/book vvakame/review /bin/bash -ci "cd /book && yarn && yarn build"
-# .review/Onestop-app-dev.pdf に出力されます。
